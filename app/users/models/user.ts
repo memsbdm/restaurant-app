488b69ce5c69c1047b37c4e6c4276dd2a7ea0dcf
Memsbdm <mehmetbdm@outlook.fr>
2024-08-07 21:26:03 +0200

    refactor: change id to uid

--- app/users/models/user.ts
+++ app/users/models/user.ts
@@ -1,17 +1,20 @@
-import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { Opaque } from '@adonisjs/core/types/helpers'
+import { DateTime } from 'luxon'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
   passwordColumnName: 'password',
 })
 
+export type UserId = Opaque<'UserId', string>
+
 export default class User extends compose(BaseModel, AuthFinder) {
   @column({ isPrimary: true })
-  declare id: number
+  declare id: UserId
 
   @column()
   declare fullName: string | null
--- database/migrations/1723057486312_create_users_table.ts
+++ database/migrations/1723057486312_create_users_table.ts
@@ -5,8 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('gen_random_uuid()').knexQuery)      table.string('full_name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
 
